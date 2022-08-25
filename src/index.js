37b3714a4d4265f977ddce39f736d804764ddab2
cynthia <cchelangat9892gmail.com>
2022-08-25 17:17:56 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-
-  let form = document.querySelector("form");
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    createToDo(e.target.new-task-description.value);
-   
-  });
-
+	createTodo()
 });
+let  createTodo= () => {
+	let tasks = document.getElementById('tasks');
+	let form = document.getElementById('create-task-form');
+	form.onsubmit = (e) => {e.preventDefault();
+		let newTask = document.getElementById('new-task-description').value;
+		let li= document.createElement('li');
+		li.appendChild(document.createTextNode(newTask));
+		tasks.appendChild(li);
+		form.reset()
+	}}
+  
+  
 
-function createToDo(e) {
-  let li = document.createElement("li");
-  li.innerText = e;
-  document.getElementById("tasks").appendChild(li);
-  let btn = document.createElement("button");
-  btn.innerText = "X";
 
-  li.appendChild(btn);
-}
-function removeTask(element) {
-  element.addEventListener('click', () => element.
-  parentNode.remove()
-  )
-}
