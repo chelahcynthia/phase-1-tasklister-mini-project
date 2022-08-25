ee34ccd45673bbf1ad41708af1213c52f016a91a
cynthia <cchelangat9892gmail.com>
2022-08-25 14:47:48 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  let form = document.getElementById("create-task-form")
+  form.addEventListener("submit" ,function(e){
+   e.preventDefault()
+  console.log(createToDo(e.target.new_task.value));
+  form.reset()
+   
+  });
+  function createToDo(todo) {
+    let li = document.createElement("li")
+    li.textContent = '${todo}';
+  }
+  
 });
+
